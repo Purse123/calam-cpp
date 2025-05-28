42bbf316c32eed3f6becf1471d9ebeeb0754002b
Pierce Neupane <sahasneupane85@gmail.com>
2025-05-28 19:21:41 +0545

    optimize for minimal code

---
+++ Canvas.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+
+typedef uint32_t u32;
+
+class Canvas {
+ private:
+  int width, height;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_Texture *texture;
+  u32* frameBuffer;
+  
+ public:
+  Canvas(int width, int height) : width(width), height(height) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      std::cerr << "[SDL ERROR] Init failed: " << SDL_GetError() << std::endl;
+      exit(1);
+    }
+
+    window = SDL_CreateWindow("Calam", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    if (!window) {
+      std::cerr << "[SDL ERROR] CreateWindow failed: " << SDL_GetError() << std::endl;
+      SDL_Quit();
+      exit(1);
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    if (!renderer) {
+      std::cerr << "[SDL ERROR] CreateRenderer failed: " << SDL_GetError() << std::endl;
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+      exit(1);
+    }
+
+    texture = SDL_CreateTexture(
+      renderer, SDL_PIXELFORMAT_ABGR8888,
+      // SDL_TEXTUREACCESS:
+      // 1. STATIC: change rarely, not locable
+      // 2. STREAMING: change frequently, locable
+      // 3. Texture used as render target
+      SDL_TEXTUREACCESS_STREAMING,
+      width, height
+    );
+    if (!texture) {
+      std::cerr << "[SDL ERROR] CreateTexture failed: " << SDL_GetError() << std::endl;
+      SDL_DestroyRenderer(renderer);
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+      exit(1);
+    }
+
+    frameBuffer = new u32[width * height];
+    memset(frameBuffer, 0, width * height * sizeof(u32));
+  }
+
+  ~Canvas() {
+    delete[] frameBuffer;
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+  }
+
+  void clear() {
+    SDL_RenderClear(renderer);	/* Clear rendering target */
+  }
+
+  void update() {
+    // rect = NULL, represent area to update, NULL for entire texture
+    // const void *pixels = frameBuffer, ray pixel data in texture format
+    // int pitch = no of byte in row including padding
+    SDL_UpdateTexture(texture, NULL, frameBuffer, width * sizeof(u32));
+  }
+
+  void present() {
+    // copy portion of current rendering target
+    // SDL_Rect* srcrect: source, dstrect: destination (NULL strech to fill given rect)
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer); /* Present rendered image to screen */
+  }
+
+  u32* getBuffer() { return frameBuffer; }
+  int getWidth() const { return width; }
+  int getHeight() const { return height; }
+
+};
---
+++ example.cpp
@@ -0,0 +1,43 @@
+#include "calam.h"
+#include <iostream>
+
+#define FACTOR 90
+#define WIDTH (16 * FACTOR)
+#define HEIGHT (9 * FACTOR)
+
+int main() {
+  Canvas canvas(WIDTH, HEIGHT);
+  Calam cfx(canvas);
+
+  // point
+  vec2D p = {300, 100};
+  RGBA cyan = {0, 255, 255, 255};
+  cfx.putPixel (p, cyan);
+
+  // line w BSA
+  vec2D p1 = {200, 400};
+  vec2D p2 = {400, 200};
+  cfx.putLine (p1, p2, cyan);
+
+  // triangle
+  vec2D t_p1 = {350, 350};
+  vec2D t_p2 = {650, 350};
+  vec2D t_p3 = {500, 500};
+  cfx.putTriangle (t_p1, t_p2, t_p3, cyan);
+
+  // circle
+  vec2D circle_center = {100, 100};
+  int radius = 50;
+  cfx.putCircle (circle_center, radius, cyan);
+
+  vec2D ellipse_center = {200, 200};
+  vec2D ellipse_radius = {100, 50};
+  cfx.putEllipse (ellipse_center, ellipse_radius, cyan);
+  
+  canvas.clear();		// clear back buffer
+  canvas.update();		// update texture in section
+  canvas.present();		// swap the buffer
+      
+  SDL_Delay(5000);
+  return 0;
+}
